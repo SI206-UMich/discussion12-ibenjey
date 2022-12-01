5bd9e7a788ff7c0d07087d4d9601433e75089ccf
Isabella Benjey <bells@macbook-pro-22.attlocal.net>
2022-11-30 21:59:47 -0500

    got through task two

--- discussion_12.py
+++ discussion_12.py
@@ -2,7 +2,7 @@ import unittest
 import sqlite3
 import json
 import os
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # starter code
 
 # Create Database
@@ -16,7 +16,9 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute('create table if not exists employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, job_id INTEGER, hire_date DATE, salary NUMBER)')
+    conn.commit
+
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,15 +29,33 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    data = json.loads(file_data)
+    for item in data:
+        emp_id = item['employee_id']
+        first_n = item['first_name']
+        last_n = item['last_name']
+        h_date = item['hire_date']
+        j_id = item['job_id']
+        income = item['salary']
+
+        cur.execute('insert or ignore into employees(employee_i,first_name,last_name,hire_date,job_id,salary)') (emp_id,first_n,last_n, h_date, j_id,income)
+        conn.commit
+
+
+
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute('select jobs.job_title,employees.hire_date from jobs join employees on jobs.job_id = employees.job_id order by employees.hire_date limit 1')
+    x = cur.fetchone()
+    return x[0]
+
+    
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
+    cur.execute('select ')
     pass
 
 # TASK 4: VISUALIZATION
